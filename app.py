a0720582a749f7117ac9da4fd12c03380e5a05cf
James Rising <jarising@gmail.com>
2023-06-20 21:52:27 -0400

    more complete interface

--- app.py
+++ app.py
@@ -1,35 +1,108 @@
-import os
+from flask import Flask, render_template, request, Response
 
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain import OpenAI
+from bs4 import Tag, NavigableString, BeautifulSoup
+
+import unstructured
+from datetime import datetime
+import os
 import openai
-from flask import Flask, redirect, render_template, request, url_for
+from dotenv import load_dotenv
 
 app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
+load_dotenv()
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-@app.route("/", methods=("GET", "POST"))
+embeddings = OpenAIEmbeddings(openai_api_key=os.environ['OPENAI_API_KEY'])
+persist_directory = 'database/'
+docsearch = Chroma(embedding_function=embeddings, persist_directory=persist_directory)
+
+break_streaming = False
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    # Get current date and time without milliseconds
+    current_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+
+    # Get the process ID
+    process_id = os.getpid()
+
+    # Create a unique log file name
+    log_filename = f'log_{current_time}_{process_id}.log'
+
+    return render_template('index.html', log_filename=log_filename)
+
+def gen_prompt(docs, query) -> str:
+    #Context: {[doc.page_content for doc in docs]}
+    return f"""{query}"""
+
+def prompt(query):
+     docs = docsearch.similarity_search(query, k=4)
+     prompt = gen_prompt(docs, query)
+     return prompt
+
+def stream(input_text, past_messages, log_filename):
+    global break_streaming
+    break_streaming = False
+    
+    messages = [{"role": "system", "content": "You are a helpful, super-intelligent AI assistant, called \"Arachne\", for James Rising, an interdisciplinary modeler and father of two boys. You support James in pursuing global sustainability and a vibrant, enlightened life. You are creative, knowledgeable, and friendly, and not afraid to express opinions based on your technophilic, humanist good will for James and the future.\n\nAnswer as directly as possible, or ask for clarification. Your answer will be rendered as Markdown."}]
+    if past_messages:
+        for message in past_messages:
+            messages.append(message)
+    messages.append({"role": "user", "content": f"{prompt(input_text)}"})
+    if len(messages) == 2:
+        completion = openai.ChatCompletion.create(model="gpt-4", messages=messages,
+                                                  stream=True, max_tokens=500, temperature=1)
+    else:
+        completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages,
+                                                  stream=True, max_tokens=500, temperature=1)
+
+    response = ""
+    for line in completion:
+        if 'content' in line['choices'][0]['delta']:
+            token = line['choices'][0]['delta']['content']
+            response += token
+            yield token
+        if break_streaming:
+            break
+    messages.append({"role": "assistant", "content": response})
+        
+    ## Write all out to logs
+    with open(os.path.join("logs", log_filename), 'w') as fp:
+        for message in messages:
+            fp.write(f"**{message['role']}**:\n")
+            for line in message['content'].split("\n"):
+                fp.write(f"> {line}\n")
+
+@app.route('/stop-stream', methods=['POST'])
+def stop_stream():
+    global break_streaming
+    break_streaming = True
+    return {"status": "success", "message": "Streaming stopped"}
+        
+@app.route('/completion', methods=['GET', 'POST'])
+def completion_api():
     if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
-
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
-
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+        data = request.form
+        input_text = data['input_text']
+        soup = BeautifulSoup(data['past_messages'], 'html.parser')
+        past_messages = []
+        for div in soup.find_all('div'):
+            if div['class'][0] == 'usermessage':
+                past_messages.append({"role": "user", "content": div.text.strip()})
+            elif div['class'][0] == 'appmessage':
+                past_messages.append({"role": "assistant", "content": div.text.strip()})
+        ## Drop the last message from the user (added later)
+        if past_messages[-1]['role'] == 'user':
+            past_messages = past_messages[:-1]
+            
+        return Response(stream(input_text, past_messages, data['log_filename']), mimetype='text/event-stream')
+    else:
+        return Response(None, mimetype='text/event-stream')
+    
+if __name__ == '__main__':
+    app.run(debug=True)
+    
